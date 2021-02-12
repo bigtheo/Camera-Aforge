fd674eab32744208a409f554ae80adedd92aeba3
Théophile KAPAPA <53141608+bigtheo@users.noreply.github.com>
2021-02-12 12:02:33 +0200

    Commit initial

---
+++ webcam Aforge/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace webcam_Aforge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.pbxImage = new System.Windows.Forms.PictureBox();
+            this.cbxWebCam = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImage)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(144, 6);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(126, 33);
+            this.BtnStart.TabIndex = 0;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // pbxImage
+            // 
+            this.pbxImage.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pbxImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbxImage.Location = new System.Drawing.Point(0, 0);
+            this.pbxImage.Name = "pbxImage";
+            this.pbxImage.Size = new System.Drawing.Size(306, 249);
+            this.pbxImage.TabIndex = 1;
+            this.pbxImage.TabStop = false;
+            // 
+            // cbxWebCam
+            // 
+            this.cbxWebCam.Dock = System.Windows.Forms.DockStyle.Top;
+            this.cbxWebCam.FormattingEnabled = true;
+            this.cbxWebCam.Location = new System.Drawing.Point(0, 0);
+            this.cbxWebCam.Name = "cbxWebCam";
+            this.cbxWebCam.Size = new System.Drawing.Size(306, 21);
+            this.cbxWebCam.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.BtnStart);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 270);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(306, 48);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pbxImage);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 21);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(306, 249);
+            this.panel2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 318);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.cbxWebCam);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImage)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.PictureBox pbxImage;
+        private System.Windows.Forms.ComboBox cbxWebCam;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ webcam Aforge/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Imaging;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace webcam_Aforge
+{
+    public partial class Form1 : Form
+    {
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCapture;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach(FilterInfo filter in filterInfoCollection)
+            {
+                cbxWebCam.Items.Add(filter.Name);
+                cbxWebCam.SelectedIndex = 0;
+                videoCapture = new VideoCaptureDevice();
+            }
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            videoCapture = new VideoCaptureDevice(filterInfoCollection[cbxWebCam.SelectedIndex].MonikerString);
+            videoCapture.NewFrame += VideoCapture_NewFrame;
+            videoCapture.Start();
+        }
+
+        private void VideoCapture_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pbxImage.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            videoCapture = new VideoCaptureDevice();
+            videoCapture.SignalToStop();
+            videoCapture.Stop();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            
+            videoCapture.SignalToStop();
+            videoCapture.Stop();
+        }
+    }
+}
